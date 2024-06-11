3915c545066e0da26061a4538e611db906d6683d
Gabrielraian <gabrielraian321@gmail.com>
2024-06-10 22:21:30 -0300

    First Commit  Projeto em andamento

---
+++ UserManagerSite.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace UserManagerSite.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
